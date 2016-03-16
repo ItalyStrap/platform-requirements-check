a05d5c0231cd1767ff1fa257e9fcccb5c7817a30
Luca Tumedei <luca@theaveragedev.com>
2016-03-16 13:55:25 +0100

    first test

---
+++ tests/wpunit/Minimum_RequirementsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Minimum_RequirementsTest extends \Codeception\TestCase\WPTestCase {
+
+	public function setUp() {
+		// before
+		parent::setUp();
+
+		// your set up methods here
+	}
+
+	public function tearDown() {
+		// your tear down methods here
+
+		// then
+		parent::tearDown();
+	}
+
+	/**
+	 * @test
+	 * it should be instantiatable
+	 */
+	public function it_should_be_instantiatable() {
+		$this->assertInstanceOf( 'Minimum_Requirements', new Minimum_Requirements( '5.2', '4.0' ) );
+	}
+}
